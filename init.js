f1ffc6bf98d9eaafc839edd1e483fb1a0755a564
Teguh Pribadi <pribadi.teguh90@gmail.com>
2012-11-25 22:03:29 +0700

    Any-Question Versi.0.1
    Tersedia tampilan awal dengan menggunakan css-junction bootmetro dan
    untuk sistem evaluasinya menggunakan Jquizzy. Pada Versi.0.1, contentnya
    masih belum sesuai, InsyaAllah akan segera rilis versi selanjutnya
    dengan berbagai *kejutan-kejutan lainnya. :)

---
+++ init.js
@@ -0,0 +1,36 @@
+﻿var init = {
+    'questions': [{
+        'question': 'jQuery is a...',
+        'answers': ['JavaScript library', 'Ruby Gem', 'PHP Framework', 'None of the above'],
+        'correctAnswer': 1
+    }, {
+        // 'question': 'X comes after?',
+        // 'answers': ['P', 'W', 'Y', 'v'],
+        // 'correctAnswer': 2
+    // }, {
+        // 'question': 'Meh stands for..',
+        // 'answers': ['Eh', '/Sigh', 'What?', 'Whatever'],
+        // 'correctAnswer': 4
+    // }, {
+        'question': '5 + 9..',
+        'answers': ['5', '9', "'14'", '14'],
+        'correctAnswer': 4
+    }, ],
+    'resultComments': {
+        // perfect: 'Albus, is that you?',
+        // excellent: 'Outstanding, noble sir!',
+        // good: 'Exceeds expectations!',
+        // average: 'Acceptable. For a muggle.',
+        // bad: 'Well, that was poor.',
+        // poor: 'Dreadful!',
+        // worst: 'For shame, troll!'
+		perfect: 'MATOH',
+        excellent: 'EXCELLENT',
+        good: 'GOOD',
+        average: 'AVERAGE',
+        bad: 'BAD',
+        poor: 'POOR',
+        worst: 'WORST'
+    }
+
+};
---
+++ ipa.js
@@ -0,0 +1,64 @@
+﻿var init = {
+    'questions': [{
+        'question': 'IPA',
+        'answers': ['JavaScript library', 'Ruby Gem', 'PHP Framework', 'None of the above'],
+        'correctAnswer': 1
+    }, {
+        'question': 'X comes after?',
+        'answers': ['P', 'W', 'Y', 'v'],
+        'correctAnswer': 2
+    }, {
+        'question': 'Meh stands for..',
+        'answers': ['Eh', '/Sigh', 'What?', 'Whatever'],
+        'correctAnswer': 4
+    }, {
+        'question': '5 + 10 = ',
+        'answers': ['5', '15', '16', '14'],
+        'correctAnswer': 2
+    }, {
+        'question': '5 + 11 = ',
+        'answers': ['16', '9', '14', '14'],
+        'correctAnswer': 1
+    },{
+        'question': '5 + 9 = ',
+        'answers': ['5', '9', '14', '14'],
+        'correctAnswer': 4
+    },{
+        'question': '6 + 9 = ',
+        'answers': ['15', '9', '14', '14'],
+        'correctAnswer': 1
+    },{
+        'question': '8 + 9 = ',
+        'answers': ['5', '9', '17', '14'],
+        'correctAnswer': 3
+    },{
+        'question': '5 + 12 = ',
+        'answers': ['5', '9', '14', '17'],
+        'correctAnswer': 4
+    },{
+        'question': '5 + 19 = ',
+        'answers': ['5', '9', '24', '14'],
+        'correctAnswer': 3
+    },{
+        'question': '5 + 29 = ',
+        'answers': ['5', '34', "'14'", '14'],
+        'correctAnswer': 2
+    },
+	],
+    'resultComments': {
+        // perfect: 'Albus, is that you?',
+        // excellent: 'Outstanding, noble sir!',
+        // good: 'Exceeds expectations!',
+        // average: 'Acceptable. For a muggle.',
+        // bad: 'Well, that was poor.',
+        // poor: 'Dreadful!',
+        // worst: 'For shame, troll!'
+		perfect: '<img src="img/gold.png"/><br>'+'MATOH',
+        excellent: '<img src="img/gold.png"/><br>'+'EXCELLENT',
+        good: '<img src="img/silver.png"/><br>'+'GOOD',
+        average: '<img src="img/bronze.png"/><br>'+'AVERAGE',
+        bad: 'BAD',
+        poor: 'POOR',
+        worst: 'WORST'
+    }
+};
